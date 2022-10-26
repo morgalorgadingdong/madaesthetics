34f80b8b915e4809ebc8ec43b3a16e4bbd026a81
Morgan <morganfolz@gmail.com>
2022-10-26 10:48:59 -0700

    initial release 1

---
+++ controllers/store.js
@@ -0,0 +1,11 @@
+
+// Update store items (post)
+    // Retrieve store items
+    // Retrieve store item images
+    // Create JSON store items for frontend
+    // Create store item pages
+// Create checkout page (post)
+    // Validate cart
+    // Generate idempotency key
+    // Create checkout page w/ square api
+    // Return checkout page to user
